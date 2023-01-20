1e2dc8abfd780b9e522b0e183f62dbe4843c458f
sguha-work <sguha1988.life@gmail.com>
2023-01-20 23:21:35 +0530

    wip

--- src/app/modules/pipes/components/purepipe/purepipe.component.ts
+++ src/app/modules/pipes/components/purepipe/purepipe.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-purepipe',
   templateUrl: './purepipe.component.html',
   styleUrls: ['./purepipe.component.scss']
 })
-export class PurepipeComponent {
+export class PurepipeComponent implements OnInit {
   public sizeInBytes: number;
+  public testTimerValue: number;
   constructor() {
     this.sizeInBytes = 1;
+    this.testTimerValue = 0;
+  }
+  ngOnInit(): void {
+    window.setInterval(()=>{
+      this.testTimerValue += 1;
+    },1000);
   }
 }
